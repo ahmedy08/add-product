6d6fcd4cf2b11ca55955cb88009c51d2a6d6832e
Ahmet <dede_dede_ahmet@hotmail.com>
2022-07-10 04:58:20 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container">
+    <h1 class="text-center">Ürün Ekleme Uygulaması</h1>
+    <hr>
+    <new-product></new-product>
+    <br><br>
+    <progress-bar></progress-bar>
+    <br><br>
+    <h3 class="text-center">Eklenen Ürünlerin Listesi</h3>
+    <hr>
+    <products></products>
+  </div>
+</template>
+
+<script>
+import NewProduct from "./components/NewProduct";
+import ProgressBar from "./components/ProgressBar";
+import Products from "./components/Products";
+
+export default {
+  components: {
+    NewProduct,
+    ProgressBar,
+    Products
+  },
+
+  methods: {}
+}
+</script>
+
+<style>
+body {
+  background-color: aliceblue;
+  padding-top: 20px;
+  padding-bottom: 20px;
+}
+
+.card {
+  margin-right: 5px;
+  margin-bottom: 5px;
+}
+
+.card:last-child {
+  margin-right: 0px;
+}
+
+.col-md-2 {
+  max-width: 15.666667% !important;
+}
+
+.product-container {
+  margin-left: 15px;
+}
+
+</style>
+
---
+++ src/components/Products.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="row product-container">
+    <product v-for="product in productList">
+      <img class="card-img-top" src="/src/assets/default.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <small><strong>Adet : </strong> 1</small>
+        <br>
+        <small><strong>Fiyat : </strong> 10</small>
+        <br>
+        <small><strong>Tutar : </strong> 10</small>
+      </div>
+    </product>
+  </div>
+</template>
+
+<script>
+import Product from "./Product";
+
+export default {
+  components: {
+    Product,
+  },
+  data() {
+    return {
+      productList: [],
+    }
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
